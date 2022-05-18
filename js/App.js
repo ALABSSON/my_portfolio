8fe203e5b7f04501d8e0578322d0036a4e8e2a5a
Alabi Yusuf <yusufolatunjialabi@gmail.com>
2022-05-18 21:41:59 +0100

    adddddd

---
+++ js/App.js
@@ -0,0 +1,11 @@
+const Menu = document.querySelector(".menu")
+const Nav = document.querySelector(".header_list")
+// const Close = document.querySelector(".menu")
+
+
+Menu.onclick = () => {
+    // Nav.style.display = "block"
+    Nav.classList.toggle("show")
+
+}
+
